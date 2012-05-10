5743abe82734fb9714d8cd0a5f6bfc6ec6af619d
Sam Russell <sr228822@gmail.com>
2012-05-09 23:16:50 -0400

    cleaning up charTest

--- final/charTest.cu
+++ final/charTest.cu
@@ -10,111 +10,6 @@
 
 extern float toBW(int bytes, float sec);
 
-void
-chartest_kernel_sequential(float* distortions, int numDistortions, int maxDistortionSize, float* target, int tWidth, int tHeight, int numLocations, float* results, int blockId, int threadId) {
-    int rangeW = tWidth - EDGE_DONT_BOTHER;
-
-    int locId = blockId * NUM_THREADS_PER_BLOCK + threadId;
-    if(locId >= numLocations)
-        return;
-
-    int tx_c = locId % rangeW;
-    int ty_c = locId / rangeW;
-
-//    int totalLocs = w_range * h_range;
-
-    float maxVal = 0.0;
-    for(int d = 0; d < numDistortions; d++) {
-        // do all of the distortions at this location
-
-        // calculate index of this distortion in buffer, pull out width/height
-        int dIndex = (d * maxDistortionSize);
-        int dWidth = int(distortions[dIndex++]);
-        int dHeight = int(distortions[dIndex++]);
-        
-        // calculate location to work on
-        int tx_0 = tx_c - (dWidth / 2);
-        int ty_0 = ty_c - (dHeight / 2);
-        if(tx_0 < 0 || ty_0 < 0 || tx_0 + dWidth >= tWidth || ty_0 + dHeight >= tHeight)
-            continue;
-
-        float sum_let = 0.0;
-        float sum_conv = 0.0;
-        for(int dy = 0; dy < dHeight; dy++) {
-            for(int dx = 0; dx < dWidth; dx++) {
-                /* calculate index into target buffer */
-                int tx = tx_0 + dx;
-                int ty = ty_0 + dy;
-                int tIndex = ty * tWidth + tx;
-
-                float t = target[tIndex];
-                float d = distortions[dIndex++];
-
-                /* Version 1
-                 *   rewards matching as a percentage of pixels present */
-#ifdef v1
-                sum_let += d;
-                sum_conv += (d * t);
-#endif
-                
-                /* Version 2
-                 *   rewards matching, punishes noise and missing */
-#ifdef v2
-                sum_let += d;
-                //float match = d*t;
-                //float noise = (1-d)*t;
-                //float missing = d*(1-t);
-                //sum_conv += (match - 0.3*noise - 0.7*missing);
-                sum_conv += (2*d*t - 0.3*t - 0.7*d);
-#endif
-
-                /* Version 3
-                 *   closeness of nearest pixel */
-#ifdef v3
-                if(d > 0.9) {
-                    sum_let += 1.0;
-                    int tRight = tIndex;
-                    int tLeft = tIndex;
-                    int tUp = tIndex;
-                    int tDown = tIndex;
-                    float pixRes;
-                    if(t > 0.9)
-                        pixRes = 1.0; 
-                    else if (target[++tRight] > 0.9 ||
-                             target[--tLeft] > 0.9 ||
-                             target[tUp -= tWidth] > 0.9 ||
-                             target[tDown += tWidth] > 0.9)
-                        pixRes = 0.8;
-                    else if (target[++tRight] > 0.9 ||
-                             target[--tLeft] > 0.9 ||
-                             target[tUp -= tWidth] > 0.9 ||
-                             target[tDown += tWidth] > 0.9)
-                        pixRes = 0.2;
-                    else if (target[++tRight] > 0.9 ||
-                             target[--tLeft] > 0.9 ||
-                             target[tUp -= tWidth] > 0.9 ||
-                             target[tDown += tWidth] > 0.9)
-                        pixRes = 0.0;
-                    else
-                        pixRes = -0.0;
-                    sum_conv += pixRes;
-                  /*  if(tx_c == 41 && ty_c == 43) {
-                        printf("matched pixel (%d,%d) at %f\n", tx, ty, pixRes);
-                    } */
-                }
-#endif
-                 
-            }
-        }
-        //float val = (float)sum_conv;
-        float val = (float)(sum_conv / sum_let);
-        val = (val < 0.0)?0.0:val;
-        maxVal = (val > maxVal)?maxVal:val;
-    }
-    results[locId] = maxVal;
-}
-
-
 __global__ void
 chartest_kernel(char* distortions, int numDistortions, int maxDistortionBytes, char* target, int tWidth, int tHeight, int numLocations, float* map) {
 //    if(dID == 0)
@@ -142,7 +37,7 @@ chartest_kernel(char* distortions, int numDistortions, int maxDistortionBytes, c
 
         // calculate index of this distortion in buffer, pull out width/height
         float * pixel = (float *) (distortions + d * maxDistortionBytes + sizeof(Image));
-	Image * header = (Image *)(distortions + d * maxDistortionBytes);
+        Image * header = (Image *)(distortions + d * maxDistortionBytes);
         int dWidth = header->width;
         int dHeight = header->height;
         
@@ -226,16 +121,6 @@ chartest_kernel(char* distortions, int numDistortions, int maxDistortionBytes, c
  * Reduce Columns Functions
  ***********************************/
 
-void reduce_columns_sequential(int rangeW, int rangeH, int row, float* map, float* results) {
-    float max = 0.0;
-    for(int c=0; c < rangeH; c++) {
-        int mapIndex = (rangeW * c) + row;
-        float thisV = map[mapIndex];
-        max = (thisV>max)?thisV:max;
-    }
-    results[row] = max;
-}
-
 __global__ void
 reduce_columns_kernel(int rangeW, int rangeH, float* map, float* results) {
     int row = threadIdx.x;
@@ -251,35 +136,6 @@ reduce_columns_kernel(int rangeW, int rangeH, float* map, float* results) {
 /************************************
  * CharTest Functions
  ***********************************/
-
-double
-charTestSequential(float * distortionsBuf, int numDistortions, int maxDistortionSize, float * targetBuf, int targetW, int targetH, int rangeW, int rangeH, float * resultBuf) {
-
-    const int threadsPerBlock = NUM_THREADS_PER_BLOCK;
-    int numLocations = rangeW * rangeH;
-    const int blocks = ((numLocations + NUM_THREADS_PER_BLOCK - 1) / NUM_THREADS_PER_BLOCK);
-    
-    const int mapBytes = numLocations * sizeof(float);
-    float * mapBuf = (float*)malloc(mapBytes);
-
-    double kernelStartTime = CycleTimer::currentSeconds();
-    for(int b = 0; b < blocks; b++) {
-        printf("Block %d/%d\n", b, blocks);
-        for(int t = 0; t < threadsPerBlock; t++) {
-            chartest_kernel_sequential(distortionsBuf, numDistortions, maxDistortionSize, targetBuf, targetW, targetH, numLocations, mapBuf, b, t);
-        }
-    }
-    double kernelEndTime = CycleTimer::currentSeconds();
-
-    for(int r = 0; r < rangeW; r++) {
-        reduce_columns_sequential(rangeW, rangeH, r, mapBuf, resultBuf);
-    }
-
-    free(mapBuf);
-    
-    return( kernelEndTime - kernelStartTime);
-
-}
 
 double
 charTest(char * distortionsBuf, int numDistortions, int maxDistortionBytes, char * device_target, int targetW, int targetH, int rangeW, int rangeH, float * resultBuf) {
@@ -368,3 +224,142 @@ printCudaInfo() {
         printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
     }
 }
+
+/************************************
+ ************************************
+ ************************************
+ * Sequential Functions for timing
+ ************************************
+ ************************************
+ ***********************************/
+
+void
+chartest_kernel_sequential(float* distortions, int numDistortions, int maxDistortionSize, float* target, int tWidth, int tHeight, int numLocations, float* results, int blockId, int threadId) {
+    int rangeW = tWidth - EDGE_DONT_BOTHER;
+
+    int locId = blockId * NUM_THREADS_PER_BLOCK + threadId;
+    if(locId >= numLocations)
+        return;
+
+    int tx_c = locId % rangeW;
+    int ty_c = locId / rangeW;
+
+//    int totalLocs = w_range * h_range;
+
+    float maxVal = 0.0;
+    for(int d = 0; d < numDistortions; d++) {
+        // do all of the distortions at this location
+
+        // calculate index of this distortion in buffer, pull out width/height
+        int dIndex = (d * maxDistortionSize);
+        int dWidth = int(distortions[dIndex++]);
+        int dHeight = int(distortions[dIndex++]);
+        
+        // calculate location to work on
+        int tx_0 = tx_c - (dWidth / 2);
+        int ty_0 = ty_c - (dHeight / 2);
+        if(tx_0 < 0 || ty_0 < 0 || tx_0 + dWidth >= tWidth || ty_0 + dHeight >= tHeight)
+            continue;
+
+        float sum_let = 0.0;
+        float sum_conv = 0.0;
+        for(int dy = 0; dy < dHeight; dy++) {
+            for(int dx = 0; dx < dWidth; dx++) {
+                /* calculate index into target buffer */
+                int tx = tx_0 + dx;
+                int ty = ty_0 + dy;
+                int tIndex = ty * tWidth + tx;
+
+                float t = target[tIndex];
+                float d = distortions[dIndex++];
+
+                /* Version 1
+                 *   rewards matching as a percentage of pixels present */
+#ifdef v1
+                sum_let += d;
+                sum_conv += (d * t);
+#endif
+                
+                /* Version 2
+                 *   rewards matching, punishes noise and missing */
+#ifdef v2
+                sum_let += d;
+                //float match = d*t;
+                //float noise = (1-d)*t;
+                //float missing = d*(1-t);
+                //sum_conv += (match - 0.3*noise - 0.7*missing);
+                sum_conv += (2*d*t - 0.3*t - 0.7*d);
+#endif
+
+                /* Version 3
+                 *   closeness of nearest pixel */
+#ifdef v3
+                sum_let += d;
+                sum_conv += (2.0*d*t - 0.2*t - 0.3*d);
+#endif
+                
+                /* Version 4
+                 *   for recaptcha with horiz/vert edge tagging */
+#ifdef v4
+                float d_v = (d > 0.5)?1.0:0.0;
+                float d_h = (d > 0.7 || (d > 0.3 && d < 0.5))?1.0:0.0;
+                
+                float t_v = (t > 0.5)?1.0:0.0;
+                float t_h = (t > 0.7 || (t > 0.3 && t < 0.5))?1.0:0.0;
+
+                sum_let += (d_v + d_h);
+                sum_conv += 2.0*(d_v * t_v) + 2.0*(d_h * t_h);
+                sum_conv -= 0.2*(t_h + t_v);
+                sum_conv -= 0.3*(d_h + d_v);
+#endif
+                 
+            }
+        }
+        //float val = (float)sum_conv;
+        float val = (float)(sum_conv / sum_let);
+        val = (val < 0.0)?0.0:val;
+        maxVal = (val > maxVal)?maxVal:val;
+    }
+    results[locId] = maxVal;
+}
+
+void reduce_columns_sequential(int rangeW, int rangeH, int row, float* map, float* results) {
+    float max = 0.0;
+    for(int c=0; c < rangeH; c++) {
+        int mapIndex = (rangeW * c) + row;
+        float thisV = map[mapIndex];
+        max = (thisV>max)?thisV:max;
+    }
+    results[row] = max;
+}
+
+double
+charTestSequential(float * distortionsBuf, int numDistortions, int maxDistortionSize, float * targetBuf, int targetW, int targetH, int rangeW, int rangeH, float * resultBuf) {
+
+    const int threadsPerBlock = NUM_THREADS_PER_BLOCK;
+    int numLocations = rangeW * rangeH;
+    const int blocks = ((numLocations + NUM_THREADS_PER_BLOCK - 1) / NUM_THREADS_PER_BLOCK);
+    
+    const int mapBytes = numLocations * sizeof(float);
+    float * mapBuf = (float*)malloc(mapBytes);
+
+    double kernelStartTime = CycleTimer::currentSeconds();
+    for(int b = 0; b < blocks; b++) {
+        printf("Block %d/%d\n", b, blocks);
+        for(int t = 0; t < threadsPerBlock; t++) {
+            chartest_kernel_sequential(distortionsBuf, numDistortions, maxDistortionSize, targetBuf, targetW, targetH, numLocations, mapBuf, b, t);
+        }
+    }
+    double kernelEndTime = CycleTimer::currentSeconds();
+
+    for(int r = 0; r < rangeW; r++) {
+        reduce_columns_sequential(rangeW, rangeH, r, mapBuf, resultBuf);
+    }
+
+    free(mapBuf);
+    
+    return( kernelEndTime - kernelStartTime);
+
+}
+
+
